f00e55e60c999175fb6b3d08af30be1053a961f3
WalterFrongia <walter.frongia@libero.it>
2021-08-05 16:36:29 +0200

    Implementazione domande su html e variabili su myscript con promt per interagire con utente, implementazione con if per calcolo sconto minorenni

--- myscript.js
+++ myscript.js
@@ -1,4 +1,35 @@
+// Algoritmo: 
+// 1. Chiedo all'utente quanti km deve percorrere. 
 
-var kilometri =prompt(" Quanti kilometri vuoi percorrere? ")
+
+var kilometri = prompt(" Quanti kilometri vuoi percorrere? ")
 
 document.getElementById("n-kilometri").innerHTML = kilometri
+
+// 2. Chiedo l'età all'utente.
+
+var etaUtente = prompt("Quanti anni hai?")
+
+document.getElementById("eta").innerHTML = etaUtente
+
+
+// 3. Calcolo prezzo del biglietto. (0,21 euro per km)
+
+var calcoloBiglietto = 0.21* kilometri 
+console.log(calcoloBiglietto);
+
+// 4. Calcolo dello sconto in base alla fascia d'età. (se utente minorenne sconto del 20%, se utente over 60 sconto del 40%, altrimenti prezzo pieno.)
+
+
+if (etaUtente< 18){
+    var sconto = calcoloBiglietto * 20 / 100
+    console.log(sconto);
+    var prezzoFinale = calcoloBiglietto - sconto
+    console.log(prezzoFinale);
+    document.getElementById("prezzo-biglietto").innerHTML = prezzoFinale
+    document.getElementById("commento-utente").innerHTML = "Sei minorenne hai uno sconto del 20%!"
+
+}
+
+
+// 5.Comunicazione del prezzo. 
